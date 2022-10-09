1101fa4101296e4abbfc216e23fa851158f1af84
AlenOrbanic <aorbanic@student.unipu.hr>
2022-10-09 19:19:23 +0200

    Ispravak zadatka 4

--- 01-zadace/zadaca-4.js
+++ 01-zadace/zadaca-4.js
@@ -1,8 +1,11 @@
 const prompt = require('prompt-sync')();
 var polje=[];
+var polje2=[];
 duzina=prompt("Upiši veličinu polja: ")
 for(let i=0; i<duzina; i++){
 broj=prompt("Upiši broj: ")
+polje2.push(broj);
 if (broj%3==0) polje.push(broj);
 }
-console.log(polje);
+console.log(polje2);
+console.log("Brojevi djeljivi s 3: ",polje);
